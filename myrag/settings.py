26a6deaf39711dde9d828850a761fe1d60b71db6
DevIsis <kitcher96@gmail.com>
2022-06-17 15:14:22 +0000

    Add templates to home app

--- myrag/settings.py
+++ myrag/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -41,7 +42,10 @@ ROOT_URLCONF = 'myrag.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, "templates", "allauth"),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
