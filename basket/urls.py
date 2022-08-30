92e71464d365e6aecc26eb8d29397d15ad320b1c
DevIsis <kitcher96@gmail.com>
2022-08-30 18:39:22 +0000

    Update durag id variable to durag info to avoid confusion add delete basket item functionality add seperate update form template

--- basket/urls.py
+++ basket/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.view_basket, name='basket'),
     path('add/<durag_id>/', views.add_to_basket, name='add_to_basket'),
     path('update/<durag_id>/', views.update_basket, name='update_basket'),
+    path('delete/<durag_id>/', views.delete_basket, name='delete_basket'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- basket/views.py
+++ basket/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.shortcuts import (
+    render, redirect, get_object_or_404, reverse, HttpResponse
+)
 from django.contrib import messages
 from products.views import view_product
 from products.models import Product
 
 
 def view_basket(request):
-    """Aview to return the basket content"""
+    """A view to return the basket content"""
 
     return render(request, "basket/basket.html")
 
@@ -14,10 +16,9 @@ def add_to_basket(request, durag_id):
     """ Add durag of a certain quantity to bag """
 
     quantity = int(request.POST.get('quantity'))
+    durag = get_object_or_404(Product, pk=durag_id)
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
-
-    print(basket[durag_id])
 
     if durag_id in list(basket.keys()):
         basket[durag_id] += quantity
@@ -31,12 +32,12 @@ def add_to_basket(request, durag_id):
     print('plus this many // else equal to this many > basket id', basket[durag_id])
     print('request basket session', request.session['basket'])
 
-    messages.success(request, "quantity test success")
+    messages.success(request, f'Added {quantity} {durag.name}')
     return redirect(view_basket)
 
 
 def update_basket(request, durag_id):
-    """ raise or lower quantity """
+    """ Raise or lower quantity """
 
     quantity = int(request.POST.get('quantity'))
     basket = request.session.get('basket', {})
@@ -44,7 +45,29 @@ def update_basket(request, durag_id):
 
     if quantity > 0:
         basket[durag_id] = quantity
-        messages.success(request, f'updated {durag.name}')
+        messages.success(request, f'Updated {durag.name}')
+    else:
+        del basket[durag_id]
+        messages.warning(request, f'Removed {durag.name}')
 
     request.session['basket'] = basket
     return redirect(view_basket)
+
+
+def delete_basket(request, durag_id):
+    """ Remove durags from the shopping bag """
+
+    try:
+        basket = request.session.get('basket', {})
+        durag = get_object_or_404(Product, pk=durag_id)
+
+        if basket[durag_id]:
+            del basket[durag_id]
+            messages.success(request, f'removed {durag.name}')
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error: {e}')
+        return HttpResponse(status=500)
