d04a915e1880ce16087849c7a4a6438eb9ac3c2a
DevIsis <kitcher96@gmail.com>
2022-09-07 13:56:46 +0000

    Create checkout form

---
+++ checkout/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'street_1', 'street_2', 'county', 'postcode',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Set placeholders, class and autofocus to first field of form
+        """
+        super().__init__(*args, **kwargs)
+        # placeholders = {
+        #     'full_name': 'Full Name',
+        #     'email': 'Email Address',
+        #     'street_1': 'Street Address 1',
+        #     'street_2': 'Street Address 2',
+        #     'county': 'County',
+        #     'postcode': 'Postcode',
+        # }
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            new_data = {
+                'placeholder': str(field),
+                'class': 'form-control'
+            }
+            self.fields[str(field)].widget.attrs.update(
+                new_data
+            )
