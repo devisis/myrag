a3ca281fc5fe1085dcae68f7ebe7a5610675ae84
DevIsis <kitcher96@gmail.com>
2022-10-27 16:47:15 +0000

    Add durag creation form

--- products/forms.py
+++ products/forms.py
@@ -1,22 +1,25 @@
-from django.forms import ModelForm
 from django import forms
-from .models import Product
-from .models import Category
-from django.core.exceptions import ValidationError
+from .models import Product, Category
 
 
-class AddProductForm(ModelForm):
+class AddProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
         fields = '__all__'
-        # widgets = {
-        #     'category',
-        #     'name',
-        #     'style',
-        #     'material',
-        #     'color',
-        #     'price',
-        #     'featured_image',
-        #     'description',
-        # }
+
+    def __init__(self, *args, **kwargs):
+        """
+        Set placeholders, class and autofocus to first field of form
+        """
+        super().__init__(*args, **kwargs)
+        cat = Category.objects.all()
+        # create list of friendly names attached to category ids
+        friendly_names = [(c.id, c.get_friendly_name()) for c in cat]
+
+        #  use freindly name as field choices instead of id
+        self.fields['category'].choices = friendly_names
+
+        # add bootstrap classes to customise fields
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'text-white border border-info rounded-sm'
