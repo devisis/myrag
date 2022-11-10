d3746393ca62d5635e0d36df89f38dd3686fb325
DevIsis <kitcher96@gmail.com>
2022-11-10 15:38:34 +0000

    Add Js validation line

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,3 +1,4 @@
+/* jshint esversion: 11, jquery: true */
 var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
